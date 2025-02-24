ec82499875bedeb270cbf0896cf444754b4280a9
Atsehaske <nadia.puczka@gmail.com>
2025-02-24 14:38:17 +0100

    Add files via upload

---
+++ deep.py
@@ -0,0 +1,5 @@
+usr = (input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")).lower().strip()
+if usr == "42" or usr == "forty-two" or usr == "forty two":
+    print("Yes")
+else:
+    print("No")
