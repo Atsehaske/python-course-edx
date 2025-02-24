c5ebe8eade464b6fe6dbbd6ecdf15bf674cc4cfe
Atsehaske <nadia.puczka@gmail.com>
2025-02-24 14:56:21 +0100

    Add files via upload

---
+++ taqueria.py
@@ -0,0 +1,31 @@
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
+def read_order():
+     total = 0
+     item =""
+     while True:
+        try:
+            item = input("Item: ").title()
+        except EOFError:
+            break
+        try:
+            total += menu[item]
+            print(f"Total: $"+ str("%.2f" % total))
+        except KeyError:
+            pass
+
+
+def main():
+    read_order()
+
+main()
